32a6b3f06d371abff0f453b8aa054285791adbe2
Jason Connell <jasontconnell@gmail.com>
2026-01-19 18:06:04 -0500

    Make list cache and interface as well

--- listcache.go
+++ listcache.go
@@ -1,22 +1,28 @@
 package cache
 
-type ListCache[T any] struct {
+type ListCache[T any] interface {
+	Get() []T
+	Store(list []T)
+	Clear()
+}
+
+type basicListCache[T any] struct {
 	data []T
 }
 
-func NewListCache[T any]() *ListCache[T] {
-	c := ListCache[T]{}
+func NewListCache[T any]() ListCache[T] {
+	c := basicListCache[T]{}
 	return &c
 }
 
-func (c *ListCache[T]) Get() []T {
+func (c *basicListCache[T]) Get() []T {
 	return c.data
 }
 
-func (c *ListCache[T]) Store(list []T) {
+func (c *basicListCache[T]) Store(list []T) {
 	c.data = list
 }
 
-func (c *ListCache[T]) Clear() {
+func (c *basicListCache[T]) Clear() {
 	c.data = nil
 }
